b29233c966d472ae3a7e294b0921d5cc04fcb0b1
lexy48 <lexy48@noreply.codeberg.org>
2023-08-20 17:03:40 +0000

    Update AniPlayProvider/build.gradle.kts

--- AniPlayProvider/build.gradle.kts
+++ AniPlayProvider/build.gradle.kts
@@ -24,5 +24,5 @@ cloudstream {
     )
 
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=aniplay.it&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=aniplay.co&sz=%size%"
 }
